78d0cb1c5bace07403ea8bc8de534ee6c5ec3059
OksanaTishchenko <oksana.tishchenko@ukr.net>
2021-01-27 21:43:04 +0200

    create footer and adaptive layout

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ var instance = new vidbg('.video', {
   webm: 'video/world.webm', // URL or relative path to webm video
   poster: 'video/poster.jpg', // URL or relative path to fallback image
   overlay: false, // Boolean to display the overlay or not
-  
+
 });
 
 var rellax = new Rellax('.rocket');
+if (document.body.clientWidth < 576) {
+  rellax.destroy(); // отключаем паралакс, если экран клиента меньше 576 пикселей
+}
